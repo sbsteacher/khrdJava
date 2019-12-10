e827e8ecc7faca84c3b8d3269ab3ccc92204e60e
jyh0131 <jyh0131@hanmail.net>
2019-12-10 11:01:43 +0900

    instanceof 사용법

--- First/src/com/kr/first/obj2/CarTest3.java
+++ First/src/com/kr/first/obj2/CarTest3.java
@@ -7,18 +7,23 @@ public class CarTest3 {
 		Ferrari fer1 = new Ferrari();
 		Car car1 = new Car("현대");
 		
-		System.out.println(cal1 instanceof Ferrari);
-		
-		
-		/*
+		//System.out.println(cal1 instanceof Ferrari);
+				
 		drive(cal1);
 		drive(fer1);
 		drive(car1);
-		*/
+		
 	}
 
 	public static void drive(Car car) {
 		car.drive();
+		if(car instanceof Ferrari) {
+			/*
+			Ferrari fer = (Ferrari)car;
+			fer.stop();
+			*/
+			((Ferrari)car).stop();
+		}
 		
 	}
 	
