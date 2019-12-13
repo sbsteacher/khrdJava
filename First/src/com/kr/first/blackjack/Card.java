9c63b8f9a4f39dfc446172f1a074873829ec0ece
jyh0131 <jyh0131@hanmail.net>
2019-12-13 10:10:34 +0900

    블랙잭 - Card

---
+++ First/src/com/kr/first/blackjack/Card.java
@@ -0,0 +1,54 @@
+package com.kr.first.blackjack;
+
+
+//무늬, 숫자, 점수
+//은닉화
+//무늬, 숫자든 값이 한번 입력되면 변경이 될 수 없도록 해주셨으면 좋겠어요!
+public class Card {
+	private final String shape;
+	private final String no;
+	private final int point;
+	
+	private static String[] shapes = {"스페이드", "하트", "클로버", "다이아몬드"};
+	
+	//bigIdx 0~3 (0:스페이드, 1:하트, 2:클로버, 3:다이아몬드)
+	//smallIdx 1~13 (1: no-A, point-1, 2: no-2, point-2
+	
+	//.... (11: no-J, point-10, 12: no-Q, point-10, 13:no-K, point-10)
+	public Card(int bigIdx, int smallIdx) {
+		super();
+		shape = shapes[bigIdx];
+		
+		if(smallIdx <= 10) {
+			point = smallIdx;
+			if(smallIdx == 1) {
+				no = "A";
+			} else {
+				no = Integer.toString(smallIdx);
+			}
+			
+		} else {
+			point = 10;
+			switch(smallIdx){
+			case 11:
+				no = "J";
+				break;
+			case 12:
+				no = "Q";
+				break;
+			default:
+				no = "K";
+				break;
+			}
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s - %s (%d)", shape, no, point);
+	}
+	
+}
+
+
+
---
+++ First/src/com/kr/first/blackjack/Game.java
@@ -0,0 +1,9 @@
+package com.kr.first.blackjack;
+
+public class Game {
+
+	public static void main(String[] args) {
+		Card c = new Card(0, 1);
+	}
+
+}
