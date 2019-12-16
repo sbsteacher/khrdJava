6074ec343b158dddae42143b23c051539a7d3056
jyh0131 <jyh0131@hanmail.net>
2019-12-16 10:33:33 +0900

    카드 더 받기

--- First/src/com/kr/first/blackjack/Game.java
+++ First/src/com/kr/first/blackjack/Game.java
@@ -1,5 +1,7 @@
 package com.kr.first.blackjack;
 
+import java.util.Scanner;
+
 public class Game {
 
 	public static void main(String[] args) {
@@ -13,6 +15,19 @@ public class Game {
 		dealer.saveCard(cd.pick());
 		
 		dealer.moreCard(cd);
+		
+		//카드를 계속 받을 수 있도록 내가 더 받기 싫을 때 까지!!!!
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			gamer.open();
+			System.out.print("카드를 더 받으시겠습니까? (y) :");
+			if(!scan.next().equalsIgnoreCase("y")) {
+				break;
+			}
+			gamer.saveCard(cd.pick());
+		}
+		scan.close();
+		
 		
 		System.out.println("--- 게이머 --- ");
 		gamer.open();
